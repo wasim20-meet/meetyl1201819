43b546440aea95963756e280666db96b2efd136a
wasim20-meet <wasim20@meet.mit.edu>
2018-10-17 12:27:27 -0400

    my second commit

---
+++ lab1.py
@@ -0,0 +1,28 @@
+# Name = "Wasim"
+# Name100 = Name * 100
+# print(Name100)
+
+# number1= 5
+# print(number1)
+# number2 = 10
+# print(number2)
+
+# i2 = 0
+# ls1 = [3,6,9]
+# i = 0
+# while i != 3:
+# 	i2 = i2 + ls1[i] 
+# 	print(ls1[i])
+# 	i = i + 1
+
+# print(i2)
+
+import turtle
+turtle.begin_fill()
+turtle.goto(0,100)
+turtle.goto(100,100)
+turtle.goto(100,0)
+turtle.goto(0,0)
+turtle.end_fill()
+turtle.mainloop()
+
