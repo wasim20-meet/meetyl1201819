345e918606e51e6e3950310036a3ff9ef582de99
wasim20-meet <wasim20@meet.mit.edu>
2018-11-21 12:02:38 -0500

    lab 6 extras 1

--- lab6.py
+++ lab6.py
@@ -2,18 +2,26 @@ import turtle
 from turtle import Turtle
 import random
 turtle.colormode(255)
-class Square(Turtle):
-	def __init__ (self, size):
+class Rectangle(Turtle):
+	def __init__ (self,x,y):
 		Turtle.__init__(self)
-		self.shape("square")
-		self.shapesize(size)
+		turtle.register_shape("rectangle",((0,0),(0,y),(x,y),(x,0),(0,0)))
+		self.shape("rectangle")
+
+# rect = Rectangle(75,100)
+class Square(Rectangle):
+	def __init__ (self):
+		Rectangle.__init__(self,50,50)
 	def random_color(self):
 		r = random.randint(0,255)
 		r1 = random.randint(0,255)
 		r2 = random.randint(0,255)
 		self.color([r,r1,r2])
-s = Square(8)
-s.random_color()
-Square = Square(3)
-Square.random_color()
+# Square = Square()
+# Square.random_color()
+
+# class Hexagon(Turtle):
+# 	def 
+
+
 turtle.mainloop()
